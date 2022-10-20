7e275c933eefea91785b5147f44af8cf936fdc07
skander bhouri <skander.bhouri@tanitweb.com>
2022-10-20 15:25:18 +0100

    QuizOver

--- src/components/Quiz/index.js
+++ src/components/Quiz/index.js
@@ -1,7 +1,10 @@
 import React, { Component } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import { QuizMarvel } from "../QuizMarvel";
 import Levels from "../Levels";
 import ProgressBar from "../ProgressBar";
+import QuizOver from "../QuizOver";
 
 class Quiz extends Component {
   state = {
@@ -10,106 +13,169 @@ class Quiz extends Component {
     maxQuestions: 10,
     storedQuestions: [],
     question: null,
-    options: [], 
+    options: [],
     idQuestion: 0,
     btnDisabled: true,
-    userAnswer: null, 
-    score: 0
+    userAnswer: null,
+    score: 0,
+    showWelcomeMsg: false,
+    quizEnd: false,
   };
-  
-  
 
-  storedDataRef = React.createRef()
+  storedDataRef = React.createRef();
 
-  loadQuestions = quizz => {
-    const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz]
-    console.log(fetchedArrayQuiz)
-    if(fetchedArrayQuiz.length >= this.state.maxQuestions){
+  loadQuestions = (quizz) => {
+    const fetchedArrayQuiz = QuizMarvel[0].quizz[quizz];
+    console.log(fetchedArrayQuiz);
+    if (fetchedArrayQuiz.length >= this.state.maxQuestions) {
+      this.storedDataRef.current = fetchedArrayQuiz;
 
-        this.storedDataRef.current = fetchedArrayQuiz
-        
-        const newArray = fetchedArrayQuiz.map( ({ answer, ...keepRest}) => keepRest)
+      const newArray = fetchedArrayQuiz.map(
+        ({ answer, ...keepRest }) => keepRest
+      );
 
-        this.setState({
-            storedQuestions: newArray
-        })
+      this.setState({
+        storedQuestions: newArray,
+      });
     } else {
-        console.log("Pas assez de questions!!!")
+      console.log("Pas assez de questions!!!");
     }
   };
 
-  componentDidMount(){
-    this.loadQuestions(this.state.levelNames[this.state.quizLevel])
+  showWelcomeMsg = (pseudo) => {
+    if (!this.state.showWelcomeMsg) {
+      this.setState({
+        showWelcomeMsg: true,
+      });
+      toast.warn(`Bienvenue ${pseudo}, et bonne chance!`, {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+      });
+    }
+  };
+
+  componentDidMount() {
+    this.loadQuestions(this.state.levelNames[this.state.quizLevel]);
   }
 
-  nextQuestion = ()=> {
-    if(this.state.idQuestion === this.state.maxQuestions - 1){
-        //end
-    } else{
-        this.setState(prevState => ({
-            idQuestion: prevState.idQuestion + 1            
-        }))
+  nextQuestion = () => {
+    if (this.state.idQuestion === this.state.maxQuestions - 1) {
+      this.gameOver();
+    } else {
+      this.setState((prevState) => ({
+        idQuestion: prevState.idQuestion + 1,
+      }));
     }
 
-    const goodAnswer = this.storedDataRef.current[this.state.idQuestion].answer
-    if (this.state.userAnswer === goodAnswer){
-        this.setState( prevState => ({
-            score: prevState.score + 1
-        }))
+    const goodAnswer = this.storedDataRef.current[this.state.idQuestion].answer;
+    if (this.state.userAnswer === goodAnswer) {
+      this.setState((prevState) => ({
+        score: prevState.score + 1,
+      }));
+
+      toast.success("Bravo +1", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    } else {
+      toast.error("Raté 0", {
+        position: "top-right",
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "colored",
+      });
+    }
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.storedQuestions !== prevState.storedQuestions) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+      });
+    }
+
+    if (this.state.idQuestion !== prevState.idQuestion) {
+      this.setState({
+        question: this.state.storedQuestions[this.state.idQuestion].question,
+        options: this.state.storedQuestions[this.state.idQuestion].options,
+        userAnswer: null,
+        btnDisabled: true,
+      });
+    }
+
+    if (this.props.userData.pseudo) {
+      this.showWelcomeMsg(this.props.userData.pseudo);
     }
   }
 
-  componentDidUpdate(prevProps, prevState){
-    if(this.state.storedQuestions !== prevState.storedQuestions){
-        
-        this.setState({
-            question: this.state.storedQuestions[this.state.idQuestion].question,
-            options: this.state.storedQuestions[this.state.idQuestion].options
-        })
-    }
-
-    if(this.state.idQuestion !== prevState.idQuestion){
-        this.setState({
-            question: this.state.storedQuestions[this.state.idQuestion].question,
-            options: this.state.storedQuestions[this.state.idQuestion].options,
-            userAnswer: null,
-            btnDisabled: true
-        })
-    }
-  }
-
-  submitAnswer = selectedAnswer => {
+  submitAnswer = (selectedAnswer) => {
     this.setState({
-        userAnswer: selectedAnswer,
-        btnDisabled: false
-    })
-  }
+      userAnswer: selectedAnswer,
+      btnDisabled: false,
+    });
+  };
+
+  gameOver = () => {
+    this.setState({
+      quizEnd: true,
+    });
+  };
 
   render() {
     //const { pseudo } = this.props.userData;
-    const displayOptions = this.state.options.map((option, index)=> {
-        return (
-            <p key={index} 
-                className={`answerOptions ${this.state.userAnswer === option ? "selected" : null}`}
-                onClick={()=>this.submitAnswer(option)}
-            >{option}</p>
-        )
-    })
-    return (
-      <div>
-        <Levels />
-        <ProgressBar />
-        <h2>{this.state.question}</h2>
-        
-        { displayOptions }
+    const displayOptions = this.state.options.map((option, index) => {
+      return (
+        <p
+          key={index}
+          className={`answerOptions ${
+            this.state.userAnswer === option ? "selected" : null
+          }`}
+          onClick={() => this.submitAnswer(option)}
+        >
+          {option}
+        </p>
+      );
+    });
 
-        <button 
-            className="btnSubmit" 
-            disabled={this.state.btnDisabled}
-            onClick={this.nextQuestion}
-        >Suivant
+    return !this.state.quizEnd ? (
+      <QuizOver ref={this.storedDataRef}/>
+    ) : (
+      <>
+        <Levels />
+        <ProgressBar 
+          idQuestion={this.state.idQuestion} 
+          maxQuestions={this.state.maxQuestions}
+        />
+        <h2>{this.state.question}</h2>
+
+        {displayOptions}
+
+        <button
+          className="btnSubmit"
+          disabled={this.state.btnDisabled}
+          onClick={this.nextQuestion}
+        >
+          {this.state.idQuestion < this.state.maxQuestions - 1 ? "Suivant" :"Terminer"}
         </button>
-      </div>
+        <ToastContainer />
+      </>
     );
   }
 }
---
+++ src/components/QuizOver/index.js
@@ -0,0 +1,54 @@
+import React, {useEffect, useState} from "react";
+
+const QuizOver = React.forwardRef((props, ref) => {
+
+  const [asked, setAsked] = useState([])
+
+  useEffect(()=>{
+    setAsked(ref.current)
+  }, [ref])
+
+  const questionAnswer = asked.map(question =>{
+    return (
+      <tr key={question.id}>
+        <td>{question.question}</td>
+        <td>{question.answer}</td>
+        <td>
+          <button className="btnInfo">Infos</button>
+        </td>
+      </tr>
+    )
+  })
+
+
+  return (
+    <>
+      <div className="stepsBtnContainer">
+        <p className="successMsg">BRAVO! PASSEZ AU NIVEAU SUIVANT</p>
+        <button className="btnResult success">Niveau Suivant</button>
+      </div>
+      <div className="percentage">
+        <div className="progressPercent">Réussite: 10%</div>
+        <div className="progressPercent">Note 10/10</div>
+      </div>
+      <hr />
+      <p>Les réponses aux questions posées</p>
+      <div className="answerContainer">
+        <table className="answers">
+          <thead>
+            <tr>
+              <th>Questions</th>
+              <th>Réponses</th>
+              <th>Infos</th>
+            </tr>
+          </thead>
+          <tbody>
+            {questionAnswer}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+});
+
+export default React.memo(QuizOver);
